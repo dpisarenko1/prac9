60cabad4e4aef469f98edd3640a97bee56c423f4
dpisarenko1 <d.pisarenko1@g.nsu.ru>
2026-03-24 20:01:05 +0700

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,14 +1,15 @@
-file = open('input5.txt', 'r', encoding = 'utf-8')
-numbers = []
+with open('input5.txt', 'r', encoding = 'utf-8') as file_in:
+    numbers = []
+    try:
+        for line in file_in:
+            numbers.append(int(line.rstrip()))
+        result = numbers[0] / numbers[1] + numbers[2]
+        open('output5.txt', 'a', encoding = 'utf-8').write(str(result))
 
-try:
-    for line in file:
-        numbers.append(int(line.rstrip()))
-    result = numbers[0] / numbers[1] + numbers[2]
-    open('output5.txt', 'a', encoding = 'utf-8').write(str(result))
+    except ValueError:
+        with open('output5.txt', 'a', encoding = 'utf-8') as file_out:
+            file_out.write('data error')
 
-except ValueError:
-    open('output5.txt', 'a', encoding = 'utf-8').write('data error')
-
-except ZeroDivisionError:
-    open('output5.txt', 'a', encoding = 'utf-8').write('division by 0')
+    except ZeroDivisionError:
+        with open('output5.txt', 'a', encoding = 'utf-8') as file_out:
+            file_out.write('division by 0')
