fad56e51c1c397307ec068d78866bda07cfefa5a
dpisarenko1 <d.pisarenko1@g.nsu.ru>
2026-03-24 20:00:16 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,5 @@
-file = open('input1.txt','r', encoding = 'utf-8')
-result = ''.join([x.upper() for x in file])
-open('output1.txt','w', encoding = 'utf-8').write(result)
+with open('input1.txt', 'r', encoding = 'utf-8') as file:
+    result = ''.join([x.upper() for x in file])
+
+    with open('output1.txt', 'w', encoding = 'utf-8') as file_out:
+        file_out.write(result)
