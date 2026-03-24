f2d0bed7c7b6418b712ec2a70c5b1f9de43bc928
dpisarenko1 <d.pisarenko1@g.nsu.ru>
2026-03-24 20:00:52 +0700

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,5 +1,5 @@
-file = open('input4.txt','r',encoding = 'utf-8')
-
-for line in file:
-    if len(line.rstrip()) > 20:
-        open('output4.txt', 'a', encoding = 'utf-8').write(line)
+with open('input4.txt', 'r', encoding = 'utf-8') as file_in:
+    for line in file_in:
+        if len(line.rstrip()) > 20:
+            with open('output4.txt', 'a', encoding = 'utf-8') as file_out:
+                file_out.write(line)
